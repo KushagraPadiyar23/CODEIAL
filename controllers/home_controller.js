38d8321b53496151bffb26549f23fe71340580fa
unknown <kushagrapadiyar23@gmail.com>
2022-09-20 16:01:41 +0530

    setup static files for layouts and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home=function(req,res){
-    return res.end('<h1>Express is up for CODEIAL</h1>');
+   // return res.end('<h1>Express is up for CODEIAL</h1>');
+    return res.render('home',{
+        title:"Home"
+    });
 }
 module.exports.posts=function(req,res){
     return res.end('<h1>you haven not posted anything yet</h1>');
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,5 @@
 module.exports.profile=function(req,res){
-    res.end('<h1>User Profile</h1>');
+    return res.render('user_profile',{title:"Profile page"});
 }
 module.exports.contact=function(req,res){
     res.end('<h1>9193346164</h1>');
